ce9305f95adbb58cbda4b7a8964f76fa86e4bda4
ajduenas69-hub <ajduenas69@gmail.com>
2026-08-24 22:21:11 +0800

    Delete session_status.php

--- session_status.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-header('Content-Type: application/json');
-
-if (!empty($_SESSION['user_id'])) {
-    echo json_encode(['loggedIn' => true, 'username' => $_SESSION['username']]);
-} else {
-    echo json_encode(['loggedIn' => false]);
-}
